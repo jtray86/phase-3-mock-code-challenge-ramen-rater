b628607ac91628859c7667fd5d2cbb78b595c300
jennifer tracy <jenniferetracy@gmail.com>
2021-02-09 09:58:25 -0500

    menu bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// write your code here
+const menuBar = document.querySelector('#ramen-menu')
+const url = "http://localhost:3000/ramens"
+
+function fetchMenuItems(){
+    fetch(url)
+    .then(res => res.json())
+    .then(data => data.forEach(item => displayMenu(item)))
+}
+fetchMenuItems()
+
+function displayMenu(item){
+    const menuImg = document.createElement("img")
+    menuImg.setAttribute("src", item.image)
+    menuImg.setAttribute("data-id", item.id)
+    console.log(menuImg)
+
+    menuBar.append(menuImg)
+}
+
+
